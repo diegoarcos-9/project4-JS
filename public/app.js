8df535453f00d5b53c587fccfbe355a026763c71
diegoarcos7 <arcos.5c@gmail.com>
2021-11-01 22:09:09 -0500

    casi terminado, problemas con el boton

--- public/app.js
+++ public/app.js
@@ -30,3 +30,62 @@ const reviews = [
     },
 ];
 
+//Select Items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random.btn');
+
+//Set starting item
+
+let currentItem = 0;
+
+//Load initial item
+window.addEventListener('DOMContentLoaded', function(){
+    showPerson();
+});
+
+//Show person based on item
+
+function showPerson(){
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+
+
+// Show next person
+
+nextBtn.addEventListener('click', function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson();
+});
+
+//Show prev person
+
+prevBtn.addEventListener('click', function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length - 1;
+    }
+    showPerson();
+});
+
+
+// Show random person
+
+randomBtn.addEventListener('click', function(){
+    currentItem = Math.floor(Math.random() * reviews.length);
+    console.log(currentItem);
+    showPerson();
+});
